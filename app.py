c78dbadabd79776d5b52983edf02a25b0200cc16
lucaslomba <lucasazevedolomba@gmail.com>
2024-04-07 21:06:23 -0300

    Add routes to login, logout and create user

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask
+from flask_login import LoginManager, login_user, login_required, logout_user
+from flask import Flask, request, jsonify
 from database import db
+import bcrypt
 
 from models.user import User
 from models.meal import Meal
@@ -8,7 +10,51 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "Your secret key"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:admin123@127.0.0.1:3306/dailydiet'
 
+login_manager = LoginManager()
 db.init_app(app)
+login_manager.init_app(app)
+
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+@app.route("/login", methods=['POST'])
+def login():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+        user = User.query.filter_by(username=username).first()
+
+        if user and bcrypt.checkpw(str.encode(password), str.encode(user.password)):
+            login_user(user)
+            return jsonify({"message": "Autenticação realizada com sucesso"})
+        
+    return jsonify({"message": "Credenciais inválidas"}), 400
+
+@app.route("/logout", methods=["GET"])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({"message": "Logout realizada com sucesso"})
+
+@app.route('/user', methods=["POST"])
+def create_user():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+        hashed_password = bcrypt.hashpw(str.encode(password), bcrypt.gensalt())
+        user = User(username=username, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        return jsonify({"message": "Usuario cadastrado com sucesso"})
+
+    return jsonify({"message": "Dados inválidas"}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
